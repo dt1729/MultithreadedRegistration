8cb720477b14c45ac425e053b353893fe33821a8
Divya Tiwari <divyatiwari1112@gmail.com>
2024-03-16 10:41:03 -0400

    Added open3d to PCL converter

--- src/PointCloudCapture.cpp
+++ src/PointCloudCapture.cpp
@@ -35,6 +35,23 @@ struct imu_data{
 struct buffer_struct{
     open3d::geometry::PointCloud point_cloud; // Can i use template here to abstract out type between open3d and pcl?
     imu_data imu_data;
+
+    pcl::PointCloud convert(){
+        const uint32_t size = point_cloud->points_.size();
+
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud = pcl::make_shared<pcl::PointCloud<pcl::PointXYZ>>();
+        pcl_cloud->width = size;
+        pcl_cloud->height = 1;
+        pcl_cloud->is_dense = false;
+        pcl_cloud->points.resize( size );
+
+        #pragma omp parallel for
+        for( int32_t i = 0; i < size; i++ ){
+            pcl_cloud->points[i].getVector3fMap() = point_cloud->points_[i].cast<float>();
+        }
+
+        return pcl_cloud;
+    }
 };
 
 /**
